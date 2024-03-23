c2789b7befb4e79be51e7f8aec30558d8479f2f5
a.durymanov <andrei.durymanov@dif.tech>
2024-03-23 22:06:18 +0100

    Final lab 1

--- assignment_0_3_correspondences_template/imagefiltering.py
+++ assignment_0_3_correspondences_template/imagefiltering.py
@@ -13,15 +13,11 @@ def get_gausskernel_size(sigma, force_odd = True):
 
 
 def gaussian1d(x: torch.Tensor, sigma: float) -> torch.Tensor: 
-    '''Function that computes values of a (1D) Gaussian with zero mean and variance sigma^2'''
-    out =  torch.zeros(x.shape)
-    return out
+    return 1 / (math.sqrt(2 * math.pi) * sigma) * (-pow(x, 2) / (2 * pow(sigma, 2))).exp()
 
 
-def gaussian_deriv1d(x: torch.Tensor, sigma: float) -> torch.Tensor:  
-    '''Function that computes values of a (1D) Gaussian derivative'''
-    out =  torch.zeros(x.shape)
-    return out
+def gaussian_deriv1d(x: torch.Tensor, sigma: float) -> torch.Tensor: 
+    return (-x / pow(sigma, 2)) * gaussian1d(x, sigma)
 
 def filter2d(x: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
     """Function that convolves a tensor with a kernel.
@@ -40,7 +36,10 @@ def filter2d(x: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
         torch.Tensor: the convolved tensor of same size and numbers of channels
         as the input.
     """
-    out =  x
+    kernelFlip = torch.flip(kernel, (0,1))
+    kernel3d = kernelFlip[None, None, :]
+    out = torch.nn.functional.conv2d(x, kernel3d, padding='same')
+
     ## Do not forget about flipping the kernel!
     ## See in details here https://towardsdatascience.com/convolution-vs-correlation-af868b6b4fb5
     
@@ -61,7 +60,15 @@ def gaussian_filter2d(x: torch.Tensor, sigma: float) -> torch.Tensor:
 
     """ 
     ksize = get_gausskernel_size(sigma)
-    out = x
+    kernel = torch.arange(-ksize, ksize)
+    kernel = kernel.reshape(1, kernel.shape[0])
+    
+    gKernel = gaussian1d(kernel, sigma=sigma)
+
+    xResult = filter2d(x, gKernel)
+    yResult = filter2d(xResult.permute(0, 1, 3, 2), gKernel)
+
+    out = yResult.permute(0, 1, 3, 2)
     return out
 
 
@@ -78,7 +85,26 @@ def spatial_gradient_first_order(x: torch.Tensor, sigma: float) -> torch.Tensor:
     """
     b, c, h, w = x.shape
     ksize = get_gausskernel_size(sigma)
-    out =  torch.zeros(b,c,2,h,w)
+    kernel = torch.arange(-ksize, ksize)
+    kernel = kernel.reshape(1, kernel.shape[0])
+
+    gKernel = gaussian1d(kernel, sigma)
+    gdKernel = gaussian_deriv1d(kernel, sigma)
+
+    out_l1 = x
+    out_l1 = filter2d(out_l1, gKernel)
+    out_l1 = filter2d(out_l1.permute(0, 1, 3, 2), gdKernel)
+    out_l1 = out_l1.permute(0, 1, 3, 2)
+
+    out_l2 = x
+    out_l2 = filter2d(out_l2, gdKernel)
+    out_l2 = filter2d(out_l2.permute(0, 1, 3, 2), gKernel)
+    out_l2 = out_l2.permute(0, 1, 3, 2)
+
+    out = torch.zeros(b,c,2,h,w)
+    out[0][0][0] = out_l1[0][0]
+    out[0][0][1] = out_l2[0][0]
+
     return out
 
 
@@ -97,6 +123,24 @@ def affine(center: torch.Tensor, unitx: torch.Tensor, unity: torch.Tensor) -> to
     assert center.size(0) == unity.size(0)
     B = center.size(0)
     out =  torch.eye(3).unsqueeze(0).repeat(B, 1, 1)
+
+    c = center[:, 0]
+    f = center[:, 1]
+    a = unitx[:, 0] - c
+    d = unitx[:, 1] - f
+    b = unity[:, 0] - c
+    e = unity[:, 1] - f
+
+    last_row = torch.tensor([0, 0, 1.]).repeat(B, 1)
+
+    out[:, 0, 0] = a
+    out[:, 0, 1] = b
+    out[:, 0, 2] = c
+    out[:, 1, 0] = d
+    out[:, 1, 1] = e
+    out[:, 1, 2] = f
+    out[:, 2] = last_row
+
     return out
 
 def extract_affine_patches(input: torch.Tensor,
@@ -123,6 +167,30 @@ def extract_affine_patches(input: torch.Tensor,
     # Note, that F.grid_sample expects coordinates in a range from -1 to 1
     # where (-1, -1) - topleft, (1,1) - bottomright and (0,0) center of the image
     out =  torch.zeros(num_patches, ch, PS, PS)
+    
+    xs = torch.linspace(-ext, ext, PS)
+    ys = torch.linspace(-ext, ext, PS)
+    x, y = torch.meshgrid(xs, ys, indexing="xy")
+    grid = torch.stack([x, y], dim=-1)
+    gx, gy, gz = grid.size()
+
+    paches = A.shape[0]
+
+    for i in range(paches):
+        t_grid = torch.zeros_like(grid)
+        
+        for gi in range(gx):
+            for gj in range(gy):
+                t = torch.matmul(A[i], torch.tensor([grid[gi][gj][0], grid[gi][gj][1], 1]))
+                t_x = t[0] / t[2] / w * 2
+                t_y = t[1] / t[2] / h * 2
+                t_grid[gi][gj] = torch.tensor([t_x, t_y])
+        t_grid = t_grid - 1
+        t_grid = t_grid.unsqueeze(0)
+        t_grid = t_grid.expand(b, gx, gy, 2)
+    
+        out[i]= torch.nn.functional.grid_sample(input[img_idxs[i]], t_grid, mode='bilinear')
+
     return out
 
 
